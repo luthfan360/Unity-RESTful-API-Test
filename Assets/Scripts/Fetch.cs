4e9d7753f2fa3d27d335b8a6fb5ed3d6ed018a17
luthfan360 <luthfan360@gmail.com>
2022-01-12 16:36:07 +0700

    It Just Works

--- Assets/Scripts/Fetch.cs
+++ Assets/Scripts/Fetch.cs
@@ -8,9 +8,9 @@ public class Fetch : MonoBehaviour
 {
     GameObject[] displays;
     Image[] buttonImage;
-    Text buttonText;
+    Text[] buttonText;
     Texture2D textureCache;
-    int randomInt;
+    //int randomInt;
     Data data = null;
     string MediaUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
     string DataUrl = "https://pokeapi.co/api/v2/pokemon/";
@@ -19,28 +19,34 @@ public class Fetch : MonoBehaviour
     {
         displays = GameObject.FindGameObjectsWithTag("Display");
         buttonImage = new Image[displays.Length];
+        buttonText = new Text[displays.Length];
 
         for (int i = 0; i < displays.Length; i++)
         {
             buttonImage[i] = displays[i].GetComponent<Image>();
+        }
+
+        for (int i = 0; i < displays.Length; i++)
+        {
+            buttonText[i] = displays[i].GetComponentInChildren<Text>();
         }  
 
-        buttonText = GameObject.Find("NameText").GetComponent<Text>();
+        //buttonText = GameObject.Find("NameText").GetComponent<Text>();
         randomize();
     }
 
     public void randomize()
     {
-        
         StartCoroutine(DownloadImage(MediaUrl));
-        StartCoroutine(GetName(DataUrl));
+        
     }
 
     IEnumerator DownloadImage(string MediaUrl)
     {   
         for (int i = 0; i < displays.Length; i++)
-        {
-            randomInt = Random.Range(1, 808);
+        { 
+            int randomInt = Random.Range(1, 808);
+            int index = i;
             string NewMediaUrl = MediaUrl + randomInt + ".png";
             UnityWebRequest request = UnityWebRequestTexture.GetTexture(NewMediaUrl);
             yield return request.SendWebRequest();
@@ -54,26 +60,30 @@ public class Fetch : MonoBehaviour
                 textureCache = DownloadHandlerTexture.GetContent(request);
                 buttonImage[i].sprite = Sprite.Create(textureCache, new Rect(0, 0, textureCache.width, textureCache.height), new Vector2(0.5f, 0.5f), 100.0f); 
             }
+
+            StartCoroutine(GetName(DataUrl, randomInt, index));   
         }       
     }
 
-    IEnumerator GetName(string DataUrl)
+    IEnumerator GetName(string DataUrl, int randomInt, int index)
     {
-        randomInt = Random.Range(1, 808);
-        string NewDataUrl = DataUrl + randomInt;
-        UnityWebRequest request = UnityWebRequest.Get(NewDataUrl);
-        yield return request.SendWebRequest();
+        // for (int i = 0; i < displays.Length; i++)
+        // {
+            string NewDataUrl = DataUrl + randomInt;
+            UnityWebRequest request = UnityWebRequest.Get(NewDataUrl);
+            yield return request.SendWebRequest();
 
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError) 
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            string json = request.downloadHandler.text;
-            data = JsonUtility.FromJson<Data>(json);
-            string pokeName = data.name;
-            buttonText.text = char.ToUpper(pokeName[0]) + pokeName.Substring(1);
-        }
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError) 
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string json = request.downloadHandler.text;
+                data = JsonUtility.FromJson<Data>(json);
+                string pokeName = data.name;
+                buttonText[index].text = char.ToUpper(pokeName[0]) + pokeName.Substring(1);
+            }
+        // }
     }
 }
